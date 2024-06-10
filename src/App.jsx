7ae7e72289c0e5e38a376b2a4f09bc1a980579b9
Chethan S <chethans17jan2004@gmail.com>
2024-06-10 19:16:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+export default function Board() {
+    return ( 
+    <>
+        <div>
+            <button className="square">1</button>
+            <button className="square">2</button>      
+            <button className="square">3</button>      
+        </div>
+        <div>
+            <button className="square">1</button>
+            <button className="square">2</button>      
+            <button className="square">3</button>      
+        </div>
+        <div>
+            <button className="square">1</button>
+            <button className="square">2</button>      
+            <button className="square">3</button>      
+        </div>
+    </>
+    )
+  }
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Square from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Square />
+  </React.StrictMode>,
+)
+
+
+
+
+
